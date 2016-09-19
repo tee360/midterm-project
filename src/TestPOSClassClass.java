148e46fd4a8aa9c6ad849b356025a8e468a69fbf
Thomas Martin <thomas.martin4@wayne.edu>
2016-09-19 09:23:58 -0400

    Got Test case for sales tax working; updated menu display

--- src/TestPOSClassClass.java
+++ src/TestPOSClassClass.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestProductClass {
+public class TestPOSClassClass {
 	
 	@Test
 	public void whenItemIs1_00Quantity1Expect1_00(){
@@ -17,6 +17,21 @@ public class TestProductClass {
 		
 		//Double lineTotal = tester.price.ge
 		Assert.assertEquals("1.0", (price.get(0) * quantity.get(0)));
+		
+	}
+	
+	@Test
+	public void whenValue100Expect6(){
+		ArrayList<Double> price = new ArrayList<>();
+		//ArrayList<Integer> quantity = new ArrayList<>();
+		
+		price.add(100.0);
+		//quantity.add(1);
+		
+		POSClass tester = new POSClass();
+		
+		//Double lineTotal = tester.price.ge
+		Assert.assertEquals(6.0, (tester.calculateSalesTax(price.get(0))));
 		
 	}
 
